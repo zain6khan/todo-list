c8c26e3bbf4b8df32a3c19331088bb9d369133ff
Zain Khan <99469190+zain6khan@users.noreply.github.com>
2022-10-30 18:42:33 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {useState} from "react";
+import "./App.css";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+
+function App() {
+  const[todos, setTodos] = useState([])
+ 
+  function addTodo(todo) {
+     setTodos([todo, ...todos]);
+   }
+
+   function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+   }
+
+   function removeTodo(id) {
+    const newTodos = todos.filter((item) => item.id !== id)
+           setTodos(newTodos)
+      }    
+ 
+   return (
+       <div className = "App">
+           <header className = "App-header">
+               <p>React Todo</p>
+               <TodoForm addTodo={addTodo}/>
+               <TodoList todos={todos} toggleComplete={toggleComplete} />
+           </header>
+       </div>
+   );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Todo from "./Todo";
+
+function TodoList({ todos, removeTodo, toggleComplete }) {
+ return (
+<ul style={{ listStyle: 'none' }}>
+     {todos.map(todo => (
+       <Todo
+         todo={todo}
+         removeTodo={removeTodo}
+         toggleComplete={toggleComplete}
+       />
+     ))}
+   </ul>
+ );
+}
+
+export default TodoList;
